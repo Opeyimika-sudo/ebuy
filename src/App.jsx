2af305304ad767c54c004f9df57c85ab77b6ed8c
Opeyimika-sudo <54380080+Opeyimika-sudo@users.noreply.github.com>
2023-01-27 13:03:06 +0100

    feat: import Routes and Route from react router and use it for the pages so far

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from 'react'
+import React from 'react'
 import HomePage from './pages/homepage/homepage.component'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 
 function App() {
+  const HatsPage = () => (
+      <div>
+        <p>Get your Hats here</p>
+      </div>
+  )
 
   return (
     <div className="App">
-      <HomePage />
+      <Routes>
+        <Route index element={<HomePage/>} />
+        <Route path="/hats" element={<HatsPage/>} />
+      </Routes>
     </div>
   )
 }
