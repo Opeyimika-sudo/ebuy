3364b39da55799891faec8b991cf054ccf8ce2aa
Opeyimika-sudo <54380080+Opeyimika-sudo@users.noreply.github.com>
2023-01-27 13:05:12 +0100

    feat: import useNavigate to switch to another page on click

--- src/components/menu_item/menu-item.component.jsx
+++ src/components/menu_item/menu-item.component.jsx
@@ -1,21 +1,27 @@
 import React from "react";
 
 import './menu-item.styles.scss'
+import { useNavigate } from "react-router-dom";
 
-const MenuItem = ({ title, imageUrl, size }) => (
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
+    const navigate = useNavigate();
+
+    return (
         <div
         className={`${size} menu_item`}
+        onClick={() => navigate(linkUrl)}
         >
                <div
                 className='background_image'
                 style={{
                 backgroundImage: `url(${imageUrl})`}}
-            />
+                />
                 <div className='content'>
                     <h1 className='title'>{title.toUpperCase()}</h1>
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
         </div>
-)
+    )
+}        
 
 export default MenuItem;
