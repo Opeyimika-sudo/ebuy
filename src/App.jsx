8a2f357d44f369a75c25f0dfa5b2b860662537e0
Opeyimika-sudo <54380080+Opeyimika-sudo@users.noreply.github.com>
2023-01-31 08:15:25 +0100

    feat: add shop page to the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
 import React from 'react'
 import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
 
 function App() {
-  const HatsPage = () => (
-      <div>
-        <p>Get your Hats here</p>
-      </div>
-  )
-
+  
   return (
     <div className="App">
       <Routes>
         <Route index element={<HomePage/>} />
-        <Route path="/hats" element={<HatsPage/>} />
+        <Route path="/shop" element={<ShopPage/>} />
       </Routes>
     </div>
   )
